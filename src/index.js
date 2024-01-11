d70c18170cc5c40cef1d3489192b90eff9e53a26
UmangGadhavana <ugadhavana@griddynamics.com>
2024-01-11 13:20:53 +0530

    rename

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
-dotenv.config({ path: "./.env" });
+dotenv.config({ path: "./Sample.env" });
 
 const port = process.env.PORT || 3000;
 
